4443749734ab7a27d88fff45de36c7f52d3c2d7d
wizgrao <gauravity@berkeley.edu>
2018-01-27 16:11:49 -0800

    mess around more

--- hello.go
+++ hello.go
@@ -2,6 +2,20 @@ package main
 
 import "fmt"
 
+func Sqrt(x float64) float64 {
+    guess := 1.0
+    for i := 0; i < 10; i++ {
+        guess -= (guess*guess - x) / (2*guess)
+    }
+    return guess
+}
+
 func main() {
     fmt.Printf("Hello, world. \n");
+    for i := 1; i <= 10; i++ {
+        for j := 1; j <= i; j ++ {
+            fmt.Printf("%v %v ", j, Sqrt(float64(j)))
+        }
+        fmt.Println()
+    }
 }
